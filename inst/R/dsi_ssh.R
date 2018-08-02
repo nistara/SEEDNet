fcde8a9db7ec5d90b30bf4d33dd682fc7f4bbdd3
Nistara <nrandhawa@ucdavis.edu>
2018-08-01 17:35:01 -0700

    add dsi ssh R file

--- inst/R/dsi_ssh.R
+++ inst/R/dsi_ssh.R
@@ -13,14 +13,14 @@ g = readRDS("inst/sampleData/flu-g.RDS")
 g = igraph::induced.subgraph(g, c("890", sample(1:1000, 200)))
 
 # *** calculate commuting rates over it
-g_comm = disnet_commuting(g)
+g_comm = disnet_commuting2(g)
 
 # *** set up the network for simulations
 for_sim = disnet_sim_setup(g_comm, seed_nd = "890", output_dir = NA)
 
+t = Sys.time()
 # *** run the simulations over the network and calc time for it too
 simres = disnet_simulate(sim_input = for_sim, sim_output_dir = NA, nsims=500)
-
-
+Sys.time() -  t
 
 
